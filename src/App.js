a4d11efc91b9279ea051b82bfc3d752ac8712136
krisbratvold <kris.bratvold@gmail.com>
2021-07-08 13:07:25 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import PersonCard from './components/personCard';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName ="John" lastName="Smith" age="88" hairColor="Brown"/>
+      <PersonCard firstName ="Kris" lastName="Bratvold" age="28" hairColor="Blonde"/>
+      <PersonCard firstName ="Millard" lastName="Guy" age="50" hairColor="Brown"/>
+      <PersonCard firstName ="Bob" lastName="Lee" age="45" hairColor="Red"/>
     </div>
   );
 }
